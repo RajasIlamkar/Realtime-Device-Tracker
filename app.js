b03bb7a4b21f2f406eda0cf8013f692da6b64f32
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2024-08-25 15:37:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+
+const PORT = 8000;
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on("connection",(socket) => {
+    socket.on("send-location",(data) =>{
+        io.emit("receive-location",{ id: socket.id, ...data });
+    });
+
+    socket.on("disconnect",() => {
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+
+server.listen(PORT, () => console.log(`Server Started At PORT ${PORT}`));
+
+
+
+
